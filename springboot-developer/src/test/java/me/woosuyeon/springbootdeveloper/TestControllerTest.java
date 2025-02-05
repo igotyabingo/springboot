1f0f37ef54f24450f26c5eb0292a93d72514a7af
igotyabingo <jenifer7933@snu.ac.kr>
2025-02-05 13:40:28 +0900

    4장 QuizControllerTest 완료, TestControllerTest 아직 미해결

--- springboot-developer/src/test/java/me/woosuyeon/springbootdeveloper/TestControllerTest.java
+++ springboot-developer/src/test/java/me/woosuyeon/springbootdeveloper/TestControllerTest.java
@@ -12,28 +12,27 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class TestControllerTest {
-
     @Autowired
     protected MockMvc mockMvc;
+
     @Autowired
     private WebApplicationContext context;
+
     @Autowired
     private MemberRepository memberRepository;
-
     @BeforeEach
-    public void mockMvcSetup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
     }
-
     @AfterEach
     public void cleanUp() {
         memberRepository.deleteAll();
@@ -42,15 +41,19 @@ class TestControllerTest {
     @DisplayName("getAllMembers: 아티클 조회에 성공한다.")
     @Test
     public void getAllMembers() throws Exception {
-        // given: 새로운 멤버를 추가하였을 때 getallmembers에 반영이 되었는지 확인한다.
+        // given
         final String url = "/test";
         Member savedMember = memberRepository.save(new Member(1L, "홍길동"));
+        // 테스트 결과: 제대로 추가 되지 않음
 
-        // when: 작업 수행 (mvc 연결)
-        final ResultActions result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON));
+        // when
+        final ResultActions result = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
 
-        // then: 검증
-        result.andExpect(status().isOk()).andExpect(jsonPath("$[0].id").value(savedMember.getId()));
+        // then
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
                 .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
     }
 }
