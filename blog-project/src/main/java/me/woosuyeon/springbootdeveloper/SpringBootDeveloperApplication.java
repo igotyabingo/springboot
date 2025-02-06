0256edbf06146cbcc94fee387a405e770917a3eb
igotyabingo <jenifer7933@snu.ac.kr>
2025-02-06 17:00:36 +0900

    블로그 프로젝트 파일 생성 및 기본 파일 수정 완료

---
+++ blog-project/src/main/java/me/woosuyeon/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,13 @@
+package me.woosuyeon.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// 프로젝트의 메인 클래스로 설정
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
