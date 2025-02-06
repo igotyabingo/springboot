eda596ba3d5b36f44a15c014a852810e8b829ee7
Suyeon Woo <125536266+igotyabingo@users.noreply.github.com>
2025-02-06 15:19:30 +0900

    조회 메소드까지만 코드 실습

--- springboot-developer/src/main/java/me/woosuyeon/springbootdeveloper/MemberRepository.java
+++ springboot-developer/src/main/java/me/woosuyeon/springbootdeveloper/MemberRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByName(String name);
 }
